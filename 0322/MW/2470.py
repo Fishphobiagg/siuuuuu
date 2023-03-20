c99ed39edaa29d155bafea1d622d841744320e8d
Fishphobiagg <dlauddn6211@icloud.com>
2023-03-21 01:34:45 +0900

    0322

--- 0322/MW/2470.py
+++ 0322/MW/2470.py
@@ -1,33 +1,22 @@
-# 두 용액
-
-'''
-0에 가까운 특성값 만들기
-두 포인터를 두고, 더해가는거..?
-두 포인터는 결국 왼쪽 구역과 오른쪽 구역을 돌아다니는거
-'''
-
 N = int(input())
 liquid = list(map(int, input().split()))
 liquid.sort()
 
-if min(liquid) > 0:
-    print(liquid[0], liquid[1])
-    exit()
-elif max(liquid) < 0:
-    print(liquid[-2], liquid[-1])
-    exit()
+min_value = liquid[0]+liquid[-1]
 left = 0
 right = N-1
-min_val = abs(liquid[0]+liquid[-1])
 ans = (0, N-1)
-while right-left > 1:
-    if abs(liquid[left+1]+liquid[right]) < abs(liquid[left]+liquid[right]):
-        left += 1
-        ans = (left+1, right)
-    elif abs(liquid[left]+liquid[right-1]) <= abs(liquid[left]+liquid[right]):
-        right -=1
-        ans = (left, right-1)
-    else:
+while left < right:
+    a = liquid[left]+liquid[right]
+    if abs(a) < abs(min_value):
+        min_value = a
         ans = (left, right)
-        break
+    if not a:
+        print(liquid[left], liquid[right])
+        exit()
+    if a > 0:
+        right -= 1
+    else:
+        left += 1
+
 print(liquid[ans[0]], liquid[ans[1]])
